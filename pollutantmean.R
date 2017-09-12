e18f40ef5da9ceb55a8ea21ed5ff5643ee424870
seungyubh <seungyubh@gmail.com>
2017-09-12 17:47:09 -0400

    elegance

--- pollutantmean.R
+++ pollutantmean.R
@@ -7,3 +7,7 @@ pollutantmean <- function (directory, pollutant, id = 1:332) {
         }
         mean(dat[[pollutant]], na.rm=TRUE)                              ##calculate ave. of indicated pollutant after removing NA
 }
+
+onelinePollutantMean <- function(directory, pollutant, id = 1:332) {
+        mean(unlist(lapply(dir(directory, full.name = TRUE), read.csv)[[1]][2:3]))
+}
