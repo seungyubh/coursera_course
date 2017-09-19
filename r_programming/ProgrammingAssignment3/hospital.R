7326fb4792b85161008e3f13c03d63b0fe3ef003
seungyubh <seungyubh@gmail.com>
2017-09-19 16:54:26 -0400

    working

---
+++ r_programming/ProgrammingAssignment3/hospital.R
@@ -0,0 +1,8 @@
+## numerous functions to rank the hospitals in specific states
+## with repect to specific disease or condition
+
+## best function will find the best hospital in a given state
+
+best <- function(state, outcome) {
+        
+}
