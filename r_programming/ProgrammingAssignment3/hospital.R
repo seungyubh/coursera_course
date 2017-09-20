125adb729ca62458bfe67902b10d1fa48ad0805e
seungyubh <seungyubh@gmail.com>
2017-09-20 10:42:45 -0400

    working

--- r_programming/ProgrammingAssignment3/hospital.R
+++ r_programming/ProgrammingAssignment3/hospital.R
@@ -4,11 +4,19 @@
 ## best function will find the best hospital in a given state
 
 best <- function(state, outcome) {
-        outcomeRead()
+        outcomeRead(state, outcome)
         
 }
 
-outcomeRead <- function() {
-        outcome <<- read.csv("outcome-of-care-measures.csv", colClasses = "character")
+## outcomeRead funciton will read in the "outcome-of-care-measures.csv"
+## and trim the data as specified
+
+outcomeRead <- function(state, outcome) {
+        rawOutcome <<- read.csv("outcome-of-care-measures.csv",
+                                colClasses = "character", na.strings = "Not Available")
+        listOfOutcome <- c("heart attack" = 11, "heart failure" = 17, "pneumonia" = 23)
+        outcomeOfInterest <- rawOutcome[,c(2,7,listOfOutcome[outcome])]
+        validOutcome <- complete.cases(outcomeOfInterest)
+        outcomeOfInterest[validOutcome,]
 }
 
